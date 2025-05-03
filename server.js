5bf7e28be628cdea267b1276f969900621934e64
Jobwafula <jobwafulabg@gmail.com>
2025-05-03 16:34:19 +0300

    changes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const fs = require("fs").promises;
 const path = require("path");
 const stringSimilarity = require("string-similarity");
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 // Configuration
